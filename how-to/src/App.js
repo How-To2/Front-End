fae904374e47bd75013fff908a28aa9eb3e499ee
Alex Martinez <alexmartinez0071234@gmail.com>
2020-03-02 15:38:15 -0500

    made the barebones form for the login

--- how-to/src/App.js
+++ how-to/src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
       <h1>How-To App</h1>
+      <Login />
     </div>
   );
 }
