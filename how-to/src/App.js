8c883b165601d935a68de6a2b03e9652d1bedd26
Alex Martinez <alexmartinez0071234@gmail.com>
2020-03-04 13:27:36 -0500

    made new component for homepage and styled it

--- how-to/src/App.js
+++ how-to/src/App.js
@@ -1,48 +1,17 @@
 import React from "react";
-
-import Navigation from './components/Navigation';
-import { BrowserRouter as Router,Link } from 'react-router-dom';
-
-import styled from "styled-components";
-
+import HomePage from "./components/HomePage";
+import Navigation from "./components/Navigation";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
-  const GradientBg = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    box-sizing: border-box;
-    width: 100%;
-    min-height: 100vh;
-    background-image: radial-gradient(circle, #fefb6b 25%, #ff9505, #ec4e20);
-  `;
-
-  const Title = styled.h1`
-    font-family: "Lobster", cursive;
-    font-size: 4rem;
-  `;
-
   return (
     <Router>
-    <div className="App">
-      <Navigation />
-      <GradientBg>
-        <Title>Knowledge for Everyone</Title>
-        <h2>Sign Up for free knowledge today!</h2>
-        <Link
-          to="/signup"
-          style={{
-            backgroundColor: "white",
-            border: "none",
-            borderRadius: "15px",
-            padding: "0.5% 2%"
-          }}
-        >
-          Click here to get started!
-        </Link>
-      </GradientBg>
-    </div>
+      <div className="App">
+        <Navigation />
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+      </div>
     </Router>
   );
 }
--- how-to/src/index.js
+++ how-to/src/index.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import logger from 'redux-logger';
 import thunk from "redux-thunk";
 
-import {howToReducer as reducer} from './reducers/howToReducer';
+import { howToReducer as reducer } from "./reducers/howToReducer";
 
-const store  = createStore(reducer, applyMiddleware(thunk, logger));
+const store = createStore(reducer, applyMiddleware(thunk));
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
