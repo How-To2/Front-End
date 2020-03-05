01af57aa94f9d414a2ca73d6e284e7fcd9dc23f7
Alex Miller <alexmillerprojects@gmail.com>
2020-03-05 13:25:31 -0800

    built edit form, adding put request

--- how-to/src/components/HowTo.js
+++ how-to/src/components/HowTo.js
@@ -32,6 +32,12 @@ const HowTo = props => {
   let guideId = useParams();
 
   const [isEditing, setIsEditing] = useState(false);
+  const [updatePost, setUpdatePost] = useState({
+    guide_name: "",
+    score: "",
+    description: "",
+    category: ""
+  });
 
   console.log(props);
 
@@ -39,46 +45,108 @@ const HowTo = props => {
     props.getPostId(guideId.id);
   }, []);
 
+  const saveEdit = e => {};
+
+  const handleChange = e => {};
+
   return (
     <Background>
       <UserPost>
-        <h1>
-          {props.singleGuide.guide ? props.singleGuide.guide.guide_name : null}
-        </h1>
-        <p>
-          {props.singleGuide.guide ? props.singleGuide.guide.description : null}
-        </p>
-        <ul className="step-list">
-          {props.singleGuide.steps ? (
-            <>
-              {props.singleGuide.steps.map(item => {
-                return (
-                  <li>
-                    <strong>{item.step_number}.</strong> {item.step_name}
-                  </li>
-                );
-              })}
-            </>
-          ) : null}
-        </ul>
-        <AuthorLikes>
-          <h4>
-            Author:{" "}
-            {props.singleGuide.guide ? props.singleGuide.guide.author : null}
-          </h4>
-          <h4>
-            Score:{" "}
-            {props.singleGuide.guide ? props.singleGuide.guide.score : null}
-          </h4>
-          {props.singleGuide.guide ? (
-            <>
-              {props.singleGuide.guide.author ===
-              localStorage.getItem("author") ? (
-                <button className="edit-btn">Edit</button>
+        {isEditing === false ? (
+          <>
+            <h1>
+              {props.singleGuide.guide
+                ? props.singleGuide.guide.guide_name
+                : null}
+            </h1>
+            <p>
+              {props.singleGuide.guide
+                ? props.singleGuide.guide.description
+                : null}
+            </p>
+            <ul className="step-list">
+              {props.singleGuide.steps ? (
+                <>
+                  {props.singleGuide.steps.map(item => {
+                    return (
+                      <li>
+                        <strong>{item.step_number}.</strong> {item.step_name}
+                      </li>
+                    );
+                  })}
+                </>
               ) : null}
-            </>
-          ) : null}
-        </AuthorLikes>
+            </ul>
+            <AuthorLikes>
+              <h4>
+                Author:{" "}
+                {props.singleGuide.guide
+                  ? props.singleGuide.guide.author
+                  : null}
+              </h4>
+              <h4>
+                Score:{" "}
+                {props.singleGuide.guide ? props.singleGuide.guide.score : null}
+              </h4>
+              {props.singleGuide.guide ? (
+                <>
+                  {props.singleGuide.guide.author ===
+                  localStorage.getItem("author") ? (
+                    <button
+                      className="edit-btn"
+                      onClick={() => setIsEditing(true)}
+                    >
+                      Edit
+                    </button>
+                  ) : null}
+                </>
+              ) : null}
+            </AuthorLikes>
+          </>
+        ) : (
+          <>
+            <form className="edit-form">
+              <h2>Edit Guide</h2>
+              <label htmlFor="guide_name">Title: </label>
+              <input
+                type="text"
+                name="guide_name"
+                value={props.singleGuide.guide.guide_name}
+              />
+              <br />
+              <label htmlFor="score">Score: </label>
+              <input
+                type="number"
+                name="score"
+                value={props.singleGuide.guide.score}
+              />{" "}
+              <br />
+              <label htmlFor="category">Category: </label>
+              <input
+                type="text"
+                name="category"
+                value={props.singleGuide.guide.category}
+              />{" "}
+              <br />
+              <label htmlFor="description">Body: </label>
+              <textarea
+                rows="5"
+                cols="10"
+                name="description"
+                value={props.singleGuide.guide.description}
+              />
+              <br />
+              <button
+                className="edit-btn"
+                onClick={() => {
+                  setIsEditing(false);
+                }}
+              >
+                Save
+              </button>
+            </form>
+          </>
+        )}
       </UserPost>
 
       <ReviewList />
